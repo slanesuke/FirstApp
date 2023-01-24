95e592a7d7d689c0fd4fb11d6036225d1c4dd07c
Ian Slane <slaneian@gmail.com>
2023-01-24 16:56:06 -0700

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Ian Slane on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
